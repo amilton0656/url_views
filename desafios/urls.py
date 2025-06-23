83a0f99d82352d0c82aa523720e12fb5d8111c54
amilton0656 <amilton0656@gmail.com>
2025-06-23 17:47:39 -0300

    2025 06 23

--- desafios/urls.py
+++ desafios/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('<int:dia>', views.desafio_semana_numero),
-    path('<str:dia>', views.desafio_semana),
+    path('<str:dia>', views.desafio_semana, name="desafio_semanal"),
 
 ]
--- desafios/views.py
+++ desafios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 desafios_dia_semana = {
     'domingo': 'Desafio de domingo',
@@ -16,11 +17,15 @@ def desafio_semana_numero(request, dia):
     if dia > len(dias) or dia == 0:
         return HttpResponseNotFound("Dia inválido")
     dia_escolhido = dias[dia -1]
-    return HttpResponseRedirect("/desafios/" + dia_escolhido)
+    redireciona_rota = reverse("desafio_semanal", args=[dia_escolhido])
+    return HttpResponseRedirect(redireciona_rota)
 
 def desafio_semana(request, dia):
     try:
         desafio = desafios_dia_semana[dia]
     except:
         return HttpResponse("Para esse dia não há desafio")
-    return HttpResponseNotFound(desafio)
+    return render(request, "desafios/desafio.html", {
+        'desafio': desafio,
+        'dia': dia.capitalize()
+    })
